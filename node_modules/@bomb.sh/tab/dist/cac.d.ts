777afed7549d86e533ccc2aac06388d0409043c2
Heinz Tuppinger <heinz.tuppinger@gmail.com>
2025-12-10 15:32:20 +0100

    remove node_modules from repository

--- node_modules/@bomb.sh/tab/dist/cac.d.ts
+++
@@ -1,8 +0,0 @@
-import { l as RootCommand } from "./t-D8yuw7Fz.js";
-import { t as CompletionConfig } from "./shared-hY8JIYei.js";
-import { CAC } from "cac";
-
-//#region src/cac.d.ts
-declare function tab(instance: CAC, completionConfig?: CompletionConfig): Promise<RootCommand>;
-//#endregion
-export { tab as default };
--- node_modules/@nuxt/cli/dist/dev/index.mjs
+++
@@ -1,9 +0,0 @@
-import "../logger-B4ge7MhP.mjs";
-import { t as initialize } from "../dev-CGEtJnLN.mjs";
-import "../kit-CPEUdXTm.mjs";
-import "../versions-CTlxnxbH.mjs";
-import "../banner-drlfl0J-.mjs";
-import "../fs-B_hD5MnA.mjs";
-import "../nuxt-Bkosdz43.mjs";
-
-export { initialize };
